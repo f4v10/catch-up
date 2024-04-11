2758ca78eace0f79dffd43b46948b890e1bed5bf
Favio Landeo <faviomsg05@gmail.com>
2024-04-10 22:50:29 -0500

    feat(news-api-client): added source and article entities.

---
+++ src/app/news/model/source.entity.ts
@@ -0,0 +1,13 @@
+export class Source {
+  id: string;
+  name: string;
+  url: string;
+  urlToLog: string;
+
+  constructor() {
+    this.id = '';
+    this.name = '';
+    this.url = '';
+    this.urlToLog = '';
+  }
+}
